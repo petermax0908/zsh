e5bde97c4c6acfcdc629a3091b0e66e91db2663c
Peter Kerschl <petermax0908@gmail.com>
2021-02-02 21:43:22 +0100

    Updated completion

--- completion.zsh
+++ completion.zsh
@@ -2,13 +2,10 @@
 autoload -U compinit
 zmodload zsh/complist
 compinit -d ~/.cache/zcompdump-$ZSH_VERSION
-
 # extended globbing (stash like '*' and '?')
 setopt extendedGlob
-
 # adds all dot files/ folders to tab completion
 _comp_options+=(globdots)
-
 # enables tab completion navigation with arrowkeys
 zstyle ':completion:*' menu select
 # tab completion mix from lower to uppercase
